ed8fe9016cf9e5dfddf6ced1d30a8ea2c8932276
Jodie Gardiner <jodiegardiner@outlook.com>
2016-10-28 16:43:47 +0100

    final release of version 1

--- project-glucose.py
+++ project-glucose.py
@@ -2,13 +2,18 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'project_glucose'
-COLLECTION_NAME = 'data_test2'
+MONGODB_URI = os.getenv('MONGODB_URI')
+# MONGODB_HOST = 'localhost'
+# MONGODB_PORT = 27017
+# DBS_NAME = 'project_glucose'
+# DBS_NAME = os.getenv('')
+# COLLECTION_NAME = 'data_test2'
+DBS_NAME = 'heroku_j94m28rx'
+COLLECTION_NAME = 'projects'
 FIELDS = {'Timestamp': True, 'BG Reading': True, 'Date': True, 'Time': True, 'Raw-Type': True, '_id': False}
 
 @app.route("/")
@@ -19,7 +24,8 @@ def index():
 
 @app.route("/glucose/projects")
 def donor_projects():
-   connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+   connection = MongoClient(MONGODB_URI)
+   # connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
    collection = connection[DBS_NAME][COLLECTION_NAME]
    projects = collection.find({"Raw-Type": "BGReceived"}, projection=FIELDS, limit=5000)
    json_projects = []
